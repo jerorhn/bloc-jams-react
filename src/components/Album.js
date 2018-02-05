1a6842a935131547b10bd1b8333b940240123dc6
Hannah Jeror <jerorhn@yahoo.com>
2018-02-05 16:57:30 -0500

    Chpt8-Assign - Add next button functionality

--- src/components/Album.js
+++ src/components/Album.js
@@ -53,6 +53,15 @@ class Album extends Component {
     this.play(newSong);
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = currentIndex + 1;
+    if (newIndex >= this.state.album.songs.length) {return};
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
+
   render() {
     return (
       <section className='album'>
@@ -92,7 +101,8 @@ class Album extends Component {
           isPlaying={this.state.isPlaying}
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-          handlePrevClick={() => this.handlePrevClick()}/>
+          handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}/>
       </section>
 
     );
